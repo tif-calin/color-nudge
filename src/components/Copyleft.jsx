5b451a6ccfdfaebdd2e6c81af4bde21acf58cd16
culi <culitif@tuta.io>
2021-10-31 19:39:11 -0700

    footer

---
+++ src/components/Copyleft.jsx
@@ -0,0 +1,12 @@
+const Copyleft = () => {
+  return (
+    <span
+      style={{
+        transform: 'scale(-1, 1)',
+        display: 'inline-block',
+      }}
+    >&copy;</span>
+  );
+};
+
+export default Copyleft;

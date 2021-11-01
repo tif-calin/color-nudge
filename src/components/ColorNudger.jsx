ee226b24f5804c224f8d54f4bc550a2fc320ef29
culi <culitif@tuta.io>
2021-11-01 14:13:58 -0700

    use colorReducer

--- src/components/ColorNudger.jsx
+++ src/components/ColorNudger.jsx
@@ -1,26 +1,50 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './ColorNudger.css';
 
-// const colorComponents = [
-//   'red', 'green', 'blue',
-//   'hue', 'saturation', 'lightness'
-// ];
+const colorComponents = [
+  'red', 'green', 'blue',
+  'hue', 'saturation', 'lightness'
+];
 
 const ColorNudger = () => {
   const dispatch = useDispatch();
   const currentColor = useSelector(state => state.color);
+  const currentLightness = useSelector(state => state.spaces.hsl.lightness);
+  const currentSteps = useSelector(state => state.steps);
+
+  const handleChangeColor = e => {
+    const { value } = e.target;
+    dispatch({ type: 'SET_COLOR', payload:  value });
+  };
+
+  const renderButton = offset => {
+    const seed = Math.round(Math.exp(offset + currentSteps, offset + currentSteps)) % Number.MAX_SAFE_INTEGER;
+
+    const decrement = seed % 2 === 0;
+    const component = colorComponents[seed % colorComponents.length];
+    const space = ['red', 'green', 'blue'].includes(component) ? 'rgb' : 'hsl';
+
+    return (
+      <button
+        onClick={e => dispatch({ type: 'NUDGE_COMPONENT', payload: { component, space, decrement } })}
+      >{decrement ? 'decrement' : 'increment'} {component} ({space})</button>
+    );
+  };
 
   return (
-    <div className="ColorNudger">
-      <span>number of steps: 0</span>
+    <div className="ColorNudger" style={{ 
+      backgroundColor: currentColor,
+      color: currentLightness > 50 ? 'black' : 'white'
+    }}>
+      <span>number of steps: {currentSteps}</span>
       <div>
-        <input type="color" onChange={e => console.log(e.target.value)}/>
-        <input type="text" value={currentColor}/>
+        <input type="color" value={currentColor} onChange={handleChangeColor}/>
+        <input type="text" value={currentColor} onChange={handleChangeColor}/>
       </div>
       <div>
-        <button>increment red</button>
-        <button>decrement hue</button>
-        <button>increment saturation</button>
+        {renderButton(5)}
+        {renderButton(53)}
+        {renderButton(157)}
       </div>
     </div>
   );
--- src/reducers/colorReducer.js
+++ src/reducers/colorReducer.js
@@ -1,24 +1,82 @@
+import { hex2rgb, hsl2rgb, rgb2hex, rgb2hsl } from '../utils/color';
 
 const initialState = {
-  color: '#000000',
-  components: {
-    red: 0,
-    green: 0,
-    blue: 0,
-    hue: 0,
-    saturation: 0,
-    lightness: 0,
+  color: '#7f7f7f',
+  steps: 0,
+  spaces: {
+    rgb: {
+      red: 127,
+      green: 127,
+      blue: 127
+    },
+    hsl: {
+      hue: 0,
+      saturation: 0,
+      lightness: 50
+    }
   }
 };
 
 const colorReducer = (state = initialState, action) => {
+  let newRGB, newHEX, newHSL;
+
+  const getUpdatedComponents = () => {
+    return {
+      ...state,
+      color: newHEX,
+      spaces: {
+        ...state.spaces,
+        rgb: {
+          red: Math.min(255, newRGB[0]),
+          green: Math.min(255, newRGB[1]),
+          blue: Math.min(255, newRGB[2])
+        },
+        hsl: {
+          hue: newHSL[0] % 360,
+          saturation: Math.min(100, newHSL[1]),
+          lightness: Math.min(100, newHSL[2])
+        }
+      }
+    };
+  }
+
   switch (action.type) {
     case 'SET_COLOR':
-      return {
-        ...state,
-        color: action.payload
-      };
-    default: return { ...state };
+      newHEX = action.payload;
+      newRGB = hex2rgb(newHEX);
+      newHSL = rgb2hsl(newRGB);
+
+      return { ...getUpdatedComponents(), steps: 0 };
+    case 'NUDGE_COMPONENT':
+      const { component, space, decrement } = action.payload;
+
+      let min = 0;
+      let max = space === 'rgb' ? 255 : 99;
+      if (component === 'hue') max = 360;
+
+      let newValue = state.spaces[space][component] + (decrement ? -1 : 1);
+      if (component === 'hue') newValue = (360 + newValue) % 360;
+      else {
+        newValue = Math.max(newValue, min);
+        newValue = Math.min(newValue, max);
+      }
+
+      const newSpace = Object.values({ ...state.spaces[space], [component]: newValue });
+
+      if (space === 'hsl') {
+        newHSL = newSpace;
+        newRGB = hsl2rgb(newHSL);
+        newHEX = rgb2hex(newRGB);
+      } else if (space === 'rgb') {
+        newRGB = newSpace;
+        newHSL = rgb2hsl(newRGB);
+        newHEX = rgb2hex(newRGB);
+      }
+
+      console.log(newHEX, newRGB, newHSL);
+      return { ...getUpdatedComponents(), steps: state.steps + 1 };
+    default: 
+      return { ...state };
   }
 };
 

87ad0b0996b93b62453c6e209503c785f7cdf6dc
culi <culitif@tuta.io>
2021-10-31 19:38:48 -0700

    make app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
+import ColorNudger from './components/ColorNudger';
+import Copyleft from './components/Copyleft';
 
 const App = () => {
   return (
     <div className="App">
-      <header></header>
+      <header>
+        <h1>React-Redux Experiments</h1>
+      </header>
+
       <main>
-        <h1>Hello World</h1>
+        {/* <section>
+          <h2>Counters, again?!</h2>
+          <div>
+            <button>-</button>
+            <input type="number"/>
+            <button>+</button>
+          </div>
+        </section> */}
+
+        <section>
+          <h2>Color Nudge</h2>
+          <ColorNudger/>
+        </section>
       </main>
-      <footer></footer>
+
+      <footer>
+        <Copyleft/> steal this
+      </footer>
     </div>
   )
 };
